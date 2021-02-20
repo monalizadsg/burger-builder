aa538631d005bbc1fa1a7d16f5c73cbf45486351
Monaliza Mateo <monalizadasig@gmail.com>
2021-02-20 15:58:12 +0800

    Implement SideDrawer component used when in mobile device

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Logo from "../../Logo/Logo";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import "./SideDrawer.css";
+
+const SideDrawer = (props) => {
+  return (
+    <React.Fragment>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={`side-drawer ${props.open ? "open" : "close"}`}>
+        <div className='side-drawer-logo'>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default SideDrawer;
