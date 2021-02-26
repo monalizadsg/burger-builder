bf383218e72f7a1095d0514e02547cca7df5390e
Monaliza Mateo <monalizadasig@gmail.com>
2021-02-26 13:38:46 +0800

    Add validation error and select input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,22 +3,71 @@ import "./Input.css";
 
 const Input = (props) => {
   let inputElement = null;
+  let validationError = null;
+  let inValidClassname = "";
 
-  switch (props.inputtype) {
+  if (props.invalid && props.shouldValidate && props.touched) {
+    inValidClassname = "invalid";
+  }
+
+  if (props.invalid && props.touched) {
+    validationError = (
+      <p className='validation-error'>Please enter a valid value!</p>
+    );
+  }
+
+  switch (props.elementType) {
     case "input":
-      inputElement = <input className='input-element' {...props} />;
+      inputElement = (
+        <input
+          className={`input-element ${inValidClassname}`}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
       break;
-    case "textare":
-      inputElement = <textarea className='input-element' {...props} />;
+    case "textarea":
+      inputElement = (
+        <textarea
+          className={`input-element ${inValidClassname}`}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+    case "select":
+      inputElement = (
+        <select
+          className={`input-element ${inValidClassname}`}
+          value={props.value}
+          onChange={props.changed}
+        >
+          {props.elementConfig.options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.displayValue}
+            </option>
+          ))}
+        </select>
+      );
       break;
     default:
-      inputElement = <input className='input-element' {...props} />;
+      inputElement = (
+        <input
+          className={`input-element ${inValidClassname}`}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
   }
 
   return (
     <div className='input'>
       <label className='label'>{props.label}</label>
       {inputElement}
+      {validationError}
     </div>
   );
 };
