4ca4a2fc75a5eab5b0d6eb5f5866fa67839be187
Monaliza Mateo <monalizadasig@gmail.com>
2021-03-01 21:42:52 +0800

    Add redux thunk to manage dispatch of an action

--- src/index.js
+++ src/index.js
@@ -5,10 +5,22 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import reducer from "./store/reducer";
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import thunk from "redux-thunk";
+import orderReducer from "./store/reducers/order";
+import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+  burgerBuilder: burgerBuilderReducer,
+  order: orderReducer,
+});
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
