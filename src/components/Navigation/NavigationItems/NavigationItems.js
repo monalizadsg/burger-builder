7741b413f201f0e1c66098a9ec5f119388d42deb
Monaliza Mateo <monalizadasig@gmail.com>
2021-03-04 10:51:11 +0800

    Implement logout

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,14 +2,20 @@ import React from "react";
 import "./NavigationItems.css";
 import NavigationItem from "./../NavigationItem/NavigationItem";
 
-const NavigationItems = () => {
+const NavigationItems = (props) => {
   return (
     <ul className='navigation-items'>
       <NavigationItem link='/' exact>
         Burger Builder
       </NavigationItem>
-      <NavigationItem link='/orders'>Orders</NavigationItem>
-      <NavigationItem link='/auth'>Authenticate</NavigationItem>
+      {props.isAuthenticated ? (
+        <NavigationItem link='/orders'>Orders</NavigationItem>
+      ) : null}
+      {!props.isAuthenticated ? (
+        <NavigationItem link='/auth'>Authenticate</NavigationItem>
+      ) : (
+        <NavigationItem link='/logout'>Logout</NavigationItem>
+      )}
     </ul>
   );
 };
