235ce2f465d2ab7f3ed4024954716f7d57a94fa5
Monaliza Mateo <monalizadasig@gmail.com>
2021-03-05 17:14:04 +0800

    Add testing using enzyme

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import ErrorHandlerContainer from "./../ErrorHandlerContainer/ErrorHandlerContai
 import { connect } from "react-redux";
 import * as actions from "./../../store/actions/index";
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
   state = {
     purchasing: false,
   };
